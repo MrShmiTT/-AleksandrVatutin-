87ea14fedf2eccdc83e026f073879c45c17e1822
Aleksandr Vatutin <alexandr.vatutin@gmail.com>
2021-10-24 14:58:56 +0300

    HW2, 6 steps done

--- src/test/java/com/epam/tc/hw2/ex1/FirstTest.java
+++ src/test/java/com/epam/tc/hw2/ex1/FirstTest.java
@@ -1,7 +1,8 @@
 package com.epam.tc.hw2.ex1;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -10,9 +11,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class FirstTest {
 
@@ -28,22 +30,52 @@ public class FirstTest {
 
     @Test
     public void firstTest() {
+        final SoftAssert softAssert = new SoftAssert();
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         webDriver.navigate().to("https://jdi-testing.github.io");
-        Assert.assertEquals("Home Page", webDriver.getTitle());
-        webElement = webDriver.findElement(By.id("user-icon"));
-        webElement.click();
-//        new WebDriverWait(webDriver, Duration.ofSeconds(10)); //explicity
+
+        //2. Assert Browser title
+        softAssert.assertEquals(webDriver.getTitle(), "Home Page");
+
+        //3. Perform login
+        webDriver.findElement(By.id("user-icon")).click();
+        //        new WebDriverWait(webDriver, Duration.ofSeconds(10)); //explicity
         webDriver.findElement(By.id("name")).sendKeys("Roman");
         webDriver.findElement(By.id("password")).sendKeys("Jdi1234" + Keys.ENTER);
 
-        webElement = webDriver.findElement(By.xpath("//span[not(@class=\"hidden\") and text()=\"Roman Iovlev\"]"));
-        Assert.assertEquals("Roman Iovlev", webElement.getText());
+        //4. Assert Username is loggined
+        softAssert.assertEquals(webDriver.findElement(By.id("user-name")).getText(), "ROMAN IOVLEV");
+
+        //5. Assert that there are 4 items on the header section are displayed, and they have proper texts
+        softAssert.assertEquals(webDriver.findElement(By.linkText("HOME")).getText(), "HOME");
+        softAssert.assertTrue(webDriver.findElement(By.linkText("HOME")).isDisplayed());
+
+        softAssert.assertEquals(webDriver.findElement(By.linkText("CONTACT FORM")).getText(), "CONTACT FORM");
+        softAssert.assertTrue(webDriver.findElement(By.linkText("CONTACT FORM")).isDisplayed());
+
+        softAssert.assertEquals(webDriver.findElement(By.linkText("SERVICE")).getText(), "SERVICE");
+        softAssert.assertTrue(webDriver.findElement(By.linkText("SERVICE")).isDisplayed());
+
+        softAssert.assertEquals(webDriver.findElement(By.linkText("METALS & COLORS")).getText(), "METALS & COLORS");
+        softAssert.assertTrue(webDriver.findElement(By.linkText("METALS & COLORS")).isDisplayed());
+
+        //6. Assert that there are 4 images on the Index Page, and they are displayed
+        List<WebElement> imgs = webDriver.findElements(By.xpath("//div[@class='benefit-icon']"));
+        for (WebElement img : imgs) {
+            softAssert.assertTrue(img.isDisplayed());
+        }
+
+        //7. Assert that there are 4 texts on the Index Page under icons, and they have proper text
+        List<WebElement> imgTexts = webDriver.findElements(By.xpath("//span[@class='benefit-txt']"));
+        for (WebElement imgText : imgTexts) {
+            softAssert.assertTrue(imgText.isDisplayed());
+        }
+
     }
 
-    @AfterMethod
+    @AfterClass
     public void clear() {
         webDriver.close();
     }
